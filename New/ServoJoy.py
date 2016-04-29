d3eacda39cee4a52c644212a9c33a4db0d26bd40
Calin Diaconu <calin_baracuda2010@yahoo.com>
2016-04-30 01:52:34 +0300

    totul merge pana acum

--- New/ServoJoy.py
+++ New/ServoJoy.py
@@ -1,40 +1,3 @@
-#servo
-
-import time
-import curses
-import atexit
-
-import pigpio
-
-SERVO = 18
-
-MIN_PW = 1000
-MID_PW = 1500
-MAX_PW = 2000
-
-def cleanup():
-   curses.nocbreak()
-   curses.echo()
-   curses.endwin()
-   pi.stop()
-
-pi = pigpio.pi()
-
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-
-atexit.register(cleanup) # Ensure original screen state is restored.
-
-in_escape = False
-in_cursor = False
-
-pulsewidth = MID_PW
-
-pi.set_servo_pulsewidth(SERVO, pulsewidth)
-
-
-
 #joystick
 import pygame
 pygame.init()
@@ -47,18 +10,15 @@ pygame.joystick.init()
 
 while done==False:
 
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
+   joystick = pygame.joystick.Joystick(0)
+   joystick.init()
 
-    axis = joystick.get_axis( 2 )
-    
-    if axis > 1:
-        pw = MAX_PW
-    else:
-        pw = MIN_PW
-        
-    if pw != pulsewidth:
-       pulsewidth = pw
-       pi.set_servo_pulsewidth(SERVO, pulsewidth)
+   axis = joystick.get_axis( 2 )
+   print (axis)
+
+   button = joystick.get_button(0)
+   
+   if button == 1:
+      done == True
 
 pygame.quit ()
--- New/servoFunctionalSper.py
+++ New/servoFunctionalSper.py
@@ -1,3 +1,5 @@
+#joystick
+
 import pygame
 
 pygame.init()
@@ -5,8 +7,42 @@ pygame.init()
 done = False
 
 pygame.joystick.init()
-    
+
+
+#servo
+
+import time
+import curses
+import atexit
+import pigpio
+
+SERVO = 18
+
+MIN_PW = 1000
+MID_PW = 1500
+MAX_PW = 2000
+
+def cleanup():
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+    pi.stop()
+
+pi = pigpio.pi()
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+
+atexit.register(cleanup)
+
+pulsewidth = MID_PW
+
+pi.set_servo_pulsewidth(SERVO, pulsewidth)
+
 while done==False:
+    pw = pulsewidth
+    
     for event in pygame.event.get():
         
         if event.type == pygame.JOYBUTTONDOWN:
@@ -27,6 +63,12 @@ while done==False:
         
         for i in range( axes ):
             axis = joystick.get_axis( i )
+            if i == 2:
+                if axis < 0:
+                    pw = MIN_PW
+                else:
+                    pw = MAX_PW
+                    print("test")
             
         buttons = joystick.get_numbuttons()
         
