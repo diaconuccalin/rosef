e6da8566d601ddf80c3c6e4b29e187575df7d8e0
Calin Diaconu <calin_baracuda2010@yahoo.com>
2016-04-30 00:47:21 +0300

    transfer

---
+++ New/ServoJoy.py
@@ -0,0 +1,41 @@
+#servo
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
+
+#joystick
+import pygame
+pygame.init()
+done = False
+
+pygame.joystick.init()
+
+
+#main loop
+
+while done==False:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONUP:
+            done=True
+
+    joystick_count = pygame.joystick.get_count()
+
+    for i in range (joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+
+        axes = joystick.get_numaxes()
+
+        for i in range ( axes ):
+            axis = joystick.get_axis( i )
+            if i == 2:
+                duty = ((float(axis)+1)/2)*18+2.5
+                pwm.ChangeDutyCycle(duty)
+                print(duty)
+
+pygame.quit ()
