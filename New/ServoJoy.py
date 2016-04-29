cf54813937fa737b5d18054e749eef73d1952f0b
Calin Diaconu <calin_baracuda2010@yahoo.com>
2016-04-30 01:02:28 +0300

    1 servo+joy stabil

--- New/ServoJoy.py
+++ New/ServoJoy.py
@@ -1,11 +1,39 @@
 #servo
-import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+import time
+import curses
+import atexit
+
+import pigpio
+
+SERVO = 18
+
+MIN_PW = 1000
+MID_PW = 1500
+MAX_PW = 2000
+
+def cleanup():
+   curses.nocbreak()
+   curses.echo()
+   curses.endwin()
+   pi.stop()
+
+pi = pigpio.pi()
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+
+atexit.register(cleanup) # Ensure original screen state is restored.
+
+in_escape = False
+in_cursor = False
+
+pulsewidth = MID_PW
+
+pi.set_servo_pulsewidth(SERVO, pulsewidth)
+
+
 
 #joystick
 import pygame
@@ -18,24 +46,19 @@ pygame.joystick.init()
 #main loop
 
 while done==False:
+
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+    axis = joystick.get_axis( 2 )
     
-    for event in pygame.event.get():
-        if event.type == pygame.JOYBUTTONUP:
-            done=True
-
-    joystick_count = pygame.joystick.get_count()
-
-    for i in range (joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-
-        axes = joystick.get_numaxes()
-
-        for i in range ( axes ):
-            axis = joystick.get_axis( i )
-            if i == 2:
-                duty = ((float(axis)+1)/2)*18+2.5
-                pwm.ChangeDutyCycle(duty)
-                print(duty)
+    if axis > 1:
+        pw = MAX_PW
+    else:
+        pw = MIN_PW
+        
+    if pw != pulsewidth:
+       pulsewidth = pw
+       pi.set_servo_pulsewidth(SERVO, pulsewidth)
 
 pygame.quit ()
