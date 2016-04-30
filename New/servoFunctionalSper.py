51c425f90599cf78368df2f64f847a7a67ef1da0
Calin Diaconu <calin_baracuda2010@yahoo.com>
2016-04-30 17:06:30 +0300

    2 servo functional

--- New/servoFunctionalSper.py
+++ New/servoFunctionalSper.py
@@ -16,8 +16,8 @@ import curses
 import atexit
 import pigpio
 
-SERVO = 18
-aux=0
+SERVO1 = 17
+SERVO2 = 18
 
 MIN_PW = 1000
 MID_PW = 1500
@@ -37,28 +37,36 @@ curses.cbreak()
 
 atexit.register(cleanup)
 
-pulsewidth = MID_PW
+pulsewidth1 = MID_PW
+pulsewidth2 = MID_PW
 
-pi.set_servo_pulsewidth(SERVO, pulsewidth)
+pi.set_servo_pulsewidth(SERVO1, pulsewidth1)
+pi.set_servo_pulsewidth(SERVO2, pulsewidth2)
 
 while done==False:
-    pw = pulsewidth
+    pw1 = pulsewidth1
+    pw2 = pulsewidth2
 
     pygame.event.get()
-            
-    joystick_count = pygame.joystick.get_count()
 
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
            
-    axis = joystick.get_axis( 0 )
-    
-    pw = pw + axis * 0.2
+    axis = joystick.get_axis( 1 )    
+    pw1 = pw1 + axis * 0.2
 
-    if pw > MAX_PW:
-        pw = MAX_PW
-    elif pw < MIN_PW:
-        pw = MIN_PW
+    axis = joystick.get_axis( 3 )
+    pw2 = pw2 + axis * 0.2
+
+    if pw1 > MAX_PW:
+        pw1 = MAX_PW
+    elif pw1 < MIN_PW:
+        pw1 = MIN_PW
+        
+    if pw2 > MAX_PW:
+        pw2 = MAX_PW
+    elif pw2 < MIN_PW:
+        pw2 = MIN_PW
 
     if joystick.get_button(9) == 1:
         done = True
@@ -68,8 +76,12 @@ while done==False:
     #for i in range( hats ):
     #    hat = joystick.get_hat( i )
 
-    if pw != pulsewidth:
-        pulsewidth = pw
-        pi.set_servo_pulsewidth(SERVO, pulsewidth)
+    if pw1 != pulsewidth1:
+        pulsewidth1 = pw1
+        pi.set_servo_pulsewidth(SERVO1, pulsewidth1)
+        
+    if pw2 != pulsewidth2:
+        pulsewidth2 = pw2
+        pi.set_servo_pulsewidth(SERVO2, pulsewidth2)
     
 pygame.quit ()
